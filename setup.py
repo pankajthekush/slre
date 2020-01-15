dc8cddd6d141119e997d0174bf36afc58015f927
pankajthekush <pankajthekush@gmail.com>
2020-01-15 17:41:23 +0530

    Updating Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='slre',
-    version='0.1',
+    version='0.2',
     packages=['slre'],
     install_requires=['beautifulsoup4>=4.8.1' , 'bs4>=0.0.1' , 'certifi>=2019.9.11','chardet>=3.0.4','idna>=2.8','psutil>=5.6.3',
 'requests>=2.22.0','selenium>=3.141.0','soupsieve>=1.9.5','urllib3>=1.25.6','pytest-shutil>=1.7.0'
