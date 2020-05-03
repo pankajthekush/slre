ab4c77f3bd41c53c858699632d7cb25444d533fc
pankajthekush <shadow@localhost.localdomain>
2020-05-03 23:46:30 +0530

    versiom bump

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup
 
 setup(
     name='slre',
-    version='1.3',
+    version='1.4',
     packages=['slre'],
-    install_requires=['bs4>=0.0.1' , 'psutil>=5.6.3',
-'requests>=2.22.0','selenium>=3.141.0']
+    install_requires=['bs4>=0.0.1' ,'requests>=2.22.0','selenium>=3.141.0']
 )
